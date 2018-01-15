d9471051190d59ea516fb5ccdd0377d617709e26
JCMUNOZ\Juan Carlos Muñoz <jcml2704@gmail.com>
2018-01-15 10:50:19 +0100

    BBDD

--- aspNetWeb/WebForm1.aspx.cs
+++ aspNetWeb/WebForm1.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
 
 
 namespace aspNetWeb
@@ -12,9 +13,26 @@ namespace aspNetWeb
     {
         usuario usuarioPrueba;
 
+
+        public WebForm1()
+        {
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+            builder.Server = "curso2018.clxnqp6pxj8t.eu-west-1.rds.amazonaws.com";
+            builder.UserID = "cursomaster";
+            builder.Password = "cursomaster2018";
+            builder.Database = "Proyecto";
+
+            MySqlConnection baseDeDatos = new MySqlConnection(builder.ToString());
+            MySqlCommand cmd = baseDeDatos.CreateCommand();
+            baseDeDatos.Open();
+            cmd.CommandText = "INSERT INTO usuario (Id,nombre,puesto,organizacion_id) values (0,'b','b',1)";
+            cmd.ExecuteNonQuery();
+
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+
         }
 
         protected void update_btn_Click(object sender, EventArgs e)
