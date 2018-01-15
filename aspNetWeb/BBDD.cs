62a7f9dbe0ad2bae351151abe7a14b4050a7899c
JCMUNOZ\Juan Carlos Muñoz <jcml2704@gmail.com>
2018-01-15 13:25:01 +0100

    GridView

---
+++ aspNetWeb/BBDD.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace aspNetWeb
+{
+    public class BBDD
+    {
+        MySqlCommand cmd;
+        MySqlConnection baseDeDatos;
+        MySqlConnectionStringBuilder builder;
+
+
+        public BBDD()
+        {
+            builder = new MySqlConnectionStringBuilder();
+            builder.Server = "curso2018.clxnqp6pxj8t.eu-west-1.rds.amazonaws.com";
+            builder.UserID = "cursomaster";
+            builder.Password = "cursomaster2018";
+            builder.Database = "Proyecto";
+
+            baseDeDatos = new MySqlConnection(builder.ToString());
+            cmd = baseDeDatos.CreateCommand();
+            baseDeDatos.Open();
+           
+        }
+
+        public void insert(string nomb,string puest, int org)
+        {
+            cmd.CommandText = "INSERT INTO usuario (Id,nombre,puesto,organizacion_id) values (0,'"+nomb +"','"+puest+"',"+org+")";
+            cmd.ExecuteNonQuery();
+        }
+
+       
+
+        public DataTable consulta()
+        {
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM usuario;", baseDeDatos);
+            
+            DataTable dataTable = new DataTable();
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+
+            da.Fill(dataTable);
+            return dataTable;
+        }
+
+    }
+}
+
+
--- aspNetWeb/WebForm1.aspx.cs
+++ aspNetWeb/WebForm1.aspx.cs
@@ -5,40 +5,32 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using MySql.Data.MySqlClient;
+using System.Data;
 
 
 namespace aspNetWeb
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
-        usuario usuarioPrueba;
-
+        
+        BBDD bdatos;
 
         public WebForm1()
         {
-            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
-            builder.Server = "curso2018.clxnqp6pxj8t.eu-west-1.rds.amazonaws.com";
-            builder.UserID = "cursomaster";
-            builder.Password = "cursomaster2018";
-            builder.Database = "Proyecto";
-
-            MySqlConnection baseDeDatos = new MySqlConnection(builder.ToString());
-            MySqlCommand cmd = baseDeDatos.CreateCommand();
-            baseDeDatos.Open();
-            cmd.CommandText = "INSERT INTO usuario (Id,nombre,puesto,organizacion_id) values (0,'b','b',1)";
-            cmd.ExecuteNonQuery();
-
+          bdatos = new BBDD();
         }
 
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            GridView1.DataSource = bdatos.consulta();
+            GridView1.DataBind();
         }
 
         protected void update_btn_Click(object sender, EventArgs e)
-        {
-            usuarioPrueba = new usuario(nombre_text.Text, puesto_text.Text, int.Parse(organizacion_text.Text));
-            
+        {   
+            bdatos.insert(nombre_text.Text, puesto_text.Text, int.Parse(organizacion_text.Text));
         }
+
+
     }
 }
